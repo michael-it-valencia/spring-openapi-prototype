3175692a4f563f7fd87b3df93b1ac2703f7fe518
michael-it-valencia <michael.it.valencia@gmail.com>
2024-06-21 08:11:31 +0800

    update

---
+++ src/test/java/com/michael/valencia/prototypes/api/template/SpringOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.valencia.prototypes.api.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringOpenApiApplicationTests {
+    
+    @Test
+	void contextLoads() {
+	}
+	
+}
